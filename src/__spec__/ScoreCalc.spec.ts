4ca2197c771a694b863cadd7b7f1c4d078c782d4
Krishan <ktanna.ri@gmail.com>
2021-02-27 21:29:26 +0000

    further test added

--- src/__spec__/ScoreCalc.spec.ts
+++ src/__spec__/ScoreCalc.spec.ts
@@ -11,3 +11,22 @@ test('given one variable, the function will return the weighted score', () => {
   const result = scoreCalculator(input);
   expect(result).toBe(16);
 });
+
+test('given an array containing multiple variables, the function will return the correct weighted score', () => {
+  const input: CalculationVariable[] = [
+    {
+      attributeScore: 64,
+      attributeWeight: 25,
+    },
+    {
+      attributeScore: 50,
+      attributeWeight: 100,
+    },
+    {
+      attributeScore: 28,
+      attributeWeight: 50,
+    },
+  ];
+  const result = scoreCalculator(input);
+  expect(result).toBe(80);
+});
