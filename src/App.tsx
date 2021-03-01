7aba9ade0cd5a2ff33e20a8e277815c313975b32
Krishan <ktanna.ri@gmail.com>
2021-03-01 21:51:57 +0000

    updated with env var

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
+import { PEXEL_API_KEY } from './constants';
 import Nav from './nav/Nav';
 import store from './redux/store';
 
 function App() {
+  console.log({ PEXEL_API_KEY });
   return (
     <Provider store={store}>
       <div className="App">
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,6 @@
 import { Attribute } from '../types/WebAppTypes';
 
-export const PEXEL_API_KEY =
-  '563492ad6f917000010000010f0474a27b29483a970441945231bb01';
+export const PEXEL_API_KEY = process.env.REACT_APP_PEXEL_API_KEY;
 
 export const sustainabilityAttribute: Attribute = {
   id: '2a76bcc4763a73ff5e337daa30ce39cdb4735450',
