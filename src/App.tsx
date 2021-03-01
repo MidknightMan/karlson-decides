edcdb387c9274e7eded3262775a483d0905d0142
Krishan <ktanna.ri@gmail.com>
2021-03-01 21:54:14 +0000

    removes c log

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
-import { PEXEL_API_KEY } from './constants';
 import Nav from './nav/Nav';
 import store from './redux/store';
 
 function App() {
-  console.log({ PEXEL_API_KEY });
   return (
     <Provider store={store}>
       <div className="App">
